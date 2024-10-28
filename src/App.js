0acb80de0f648e8a3ca1557aa295648af1d82129
MTFL1 <m.ecomplus1@gmail.com>
2024-10-28 15:38:52 +0100

    center h1

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Evenements from './pages/Evenements/Evenements';
 import AntreConnaissance from './pages/AntreConnaissance/AntreConnaissance';
 import EntreConnaissance from './pages/EntreConnaissance/EntreConnaissance';
 import Contact from './pages/Contact/Contact';
-import MawlidAnNabi from './pages/Mawlid/Mawlid-An-Nabi';
 import Error404 from './pages/Error404/Error404';
+
+//import MawlidAnNabi from './pages/Mawlid/Mawlid-An-Nabi';
 
 
 import './App.css';
@@ -28,7 +29,6 @@ function App() {
         <Route path="/entreConnaissance" element={<EntreConnaissance />} />
         <Route path="/pageEvenementAlaUne" element={<PageEvenementAlaUne />} />
         <Route path='/pageEvenementIntroductionAlaMeditationSoufie' element={<PageEvenementIntroductionAlaMeditationSoufie />} />
-        <Route path="/mawlidAnNabi" element={<MawlidAnNabi />} />
         <Route path="/contact" element={<Contact />} /> 
         <Route path="*" element={<Error404 />} />
     </Routes>
