fd21c8035dd545cd4b3ddf94f1a30ab94e27428b
MTFL1 <m.ecomplus1@gmail.com>
2024-11-07 21:51:43 +0100

    modif card

--- src/components/ProchainEvent.jsx
+++ src/components/ProchainEvent.jsx
@@ -1,39 +1,20 @@
 import React from "react";
+import Flyer1 from "../img/1611.webp"
+
 
 function ProchainEvent() {
   const events = [
-    { day: "2", month: "Nov", title: "Introduction à la méditation soufie", href: "/pageEvenementIntroductionAlaMeditationSoufie", isLarge: true },
-    { day: "16", month: "Nov", title: "Le soufisme : une voie d'introspection", href: "/pageEvenementAlaUne", isLarge: true }
-  ];
+    {
+        title: "Flyer",
+        img: Flyer1,
+        href: "/pageEvenementAlaUne"
+    }
+    
+]
 
-  const EventCard = ({ day, month, title, href, isLarge }) => (
-    <a
-      href={href || "#"}
-      className={`flex grow p-8 my-4 mx-auto w-[100%] text-white bg-gray-900 rounded-3xl md:flex-wrap md:mt-4 md:max-w-full ${isLarge ? 'pb-12' : ''}`}
-      
-      rel="noopener noreferrer"
-    >
-      <div className="flex flex-auto gap-4 w-[90%] items-start">
-        <div className="flex flex-col font-medium whitespace-nowrap">
-          <div className="text-4xl leading-[48px] md:text-3xl">{day}</div>
-          <div className="text-xl uppercase tracking-wide">{month}</div>
-          <div className="shrink-0 self-start w-10 h-0.5 bg-white" />
-        </div>
-        <div className={`flex flex-col grow shrink-0 mt-2 text-2xl font-bold leading-8 basis-0 w-fit ${isLarge ? 'flex-auto self-start mt-1.5' : ''} md:text-xl md:leading-6`}>
-          <div className={isLarge ? "flex-auto self-start md:text-3xl " : ""}>{title}</div>
-        </div>
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/622a48f5953e4efcbd93191a00c17ef1745e80759f04c985d0ec663268135304?apiKey=aa921c22d5e8470a9701bc1a79b29d5d&"
-          alt=""
-          className={`shrink-0 w-12 aspect-square ${isLarge ? 'self-end mt-4' : 'my-auto'}`}
-        />
-      </div>
-    </a>
-  );
 
   return (
-    <section className="relative section-events flex flex-col justify-center items-center px-4 py-6 md:px-8 mt-12 w-full md:px-2 md:mt-10 md:max-w-full">
+    <section className="relative">
       <div className="petal absolute inset-0">
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/077526e80df6595ab71c00788b3a9e12ea1dfdd7ed1017c00c1c9e91f217aab0?apiKey=aa921c22d5e8470a9701bc1a79b29d5d&" alt="" className="absolute top-5 left-14 w-9 aspect-[0.67] fill-yellow-600 fill-opacity-20" />
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/82d0cc958661239053bacf859eb20cb7705bd8eaaa3a7ec83096b7f29ce6c224?apiKey=aa921c22d5e8470a9701bc1a79b29d5d&" alt="" className="absolute shrink-0 bottom-2 ml-14 aspect-[0.56] fill-teal-300 fill-opacity-20 w-[40px] max-md:mt-10" />
@@ -42,25 +23,28 @@ function ProchainEvent() {
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/077526e80df6595ab71c00788b3a9e12ea1dfdd7ed1017c00c1c9e91f217aab0?apiKey=aa921c22d5e8470a9701bc1a79b29d5d&" alt="" className="absolute top-52 right-16 rotate-45 w-9 aspect-[0.67] fill-yellow-600 fill-opacity-20" />
         <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/077526e80df6595ab71c00788b3a9e12ea1dfdd7ed1017c00c1c9e91f217aab0?apiKey=aa921c22d5e8470a9701bc1a79b29d5d&" alt="" className="absolute top-6 right-1/4 w-9 aspect-[0.67] fill-yellow-600 fill-opacity-20" />
       </div>
-      <div className="relative z-10 flex items-center w-full space-x-3 max-w-screen-xl mx-auto">
-        <span className="block w-10 h-1 bg-cyan-600"></span>
-        <h3 className="text-gray-900 font-bold">NOS PROCHAINS ÉVÈNEMENTS </h3>
-      </div>
-      <div className="relative z-10 mt-6 w-full max-w-screen-xl mx-auto">
-        <div className="flex flex-col md:flex-row md:gap-4">
-          {events.map((event, index) => (
-            <div className="flex flex-col w-full md:w-1/2" key={index}>
-              <EventCard
-                day={event.day}
-                month={event.month}
-                title={event.title}
-                href={event.href}
-                isLarge={event.isLarge}
-              />
+      <div className="relative z-10 max-w-screen-xl mx-auto px-4 md:py-12">
+                <div className="flex items-center space-x-3">
+                    <span className="block w-10 h-1 bg-cyan-600"></span>
+                    <h3 className="text-gray-900 font-bold">NOS PROCHAINS ÉVÈNEMENTS</h3>
+                </div>
+                <ul className="grid gap-x-8 gap-y-10 mt-6 sm:grid-cols-2 lg:grid-cols-3">
+                    {events.map((items, key) => (
+                        <li className="w-full mx-auto group sm:max-w-sm pb-4" key={key}>
+                            <a href={items.href}>
+                                <div className="relative w-full pt-[140%]"> {/* Proportion ajustée */}
+                                    <img
+                                        src={items.img}
+                                        loading="lazy"
+                                        alt={items.title}
+                                        className="absolute inset-0 shadow-md w-full h-full object-fill rounded-lg"
+                                    />
+                                </div>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
             </div>
-          ))}
-        </div>
-      </div>
     </section>
   );
 }
