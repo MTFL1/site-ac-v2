7283af0935f2b8cd27a5707463816b0307c6518a
MTFL1 <m.ecomplus1@gmail.com>
2025-01-26 17:21:29 +0100

    add presentation de l'intervenant

--- src/pages/PageEvenement/MusiqueEtSpiritualiteMars/ui/DetailsevenementsMars.jsx
+++ src/pages/PageEvenement/MusiqueEtSpiritualiteMars/ui/DetailsevenementsMars.jsx
@@ -82,17 +82,14 @@ function DetailsEvenements() {
       </div>
 
       <div className="relative flex flex-col w-full mx-auto text-gray-900 py-10 px-5 md:w-[60%]">
-        {/* Titre 
-        <h1 className="text-4xl font-bold mb-6">Art & spiritualité :  Poésie et nécessité intérieure</h1>
+        {/* Titre */}
+        <h1 className="text-4xl font-bold mb-6">Qui est Marc Loopuyt ?</h1>
 
-        {/* Premier paragraphe
+        {/* Premier paragraphe*/}
         <p className="text-justify leading-7 mb-6">
-        De quel besoin naît la poésie ?<br />
-Un proverbe japonais nous dit : « Quand brille la lune, le plus malheureux n'est pas l'aveugle, mais le muet. » Peut-être ce malheur est-il la douloureuse frustration qui nous saisit lorsque nous ne parvenons pas à rendre hommage, par les mots, à une beauté qui nous ravit. La poésie ne pourrait-elle pas se comprendre comme l’effort d’inventer un « sur-langage », pour traduire une expérience que les mots du quotidien ne parviennent pas à décrire ? Un langage du coeur qui évoquerait sans enfermer, qui dévoilerait sans abîmer. Son caractère allusif et symbolique permettrait de révéler ce qui, dans le monde visible et matériel, est porteur d’un sens caché. La nuit étoilée, le sourire d’un visage aimé, le levain du pain qui gonfle, l’eau transparente qui lave et abreuve… 
-    Mais le poète ne saurait révéler ces beautés sans être intérieurement disposé à les recevoir. S’il peut entrevoir la part symbolique du monde sensible, c’est au prix d’un détachement de lui-même qui le rend pleinement présent. Comme l’écrit l’auteur Sosêki : « savoir observer la vie, s’abstraire de toute préoccupation et balayer les contraintes de l’égoïsme » sont les conditions pour parvenir au regard poétique. 
-    <br />De quelle exigence intérieure la poésie est-elle l’expression ? Quelles sont les conditions nécessaires à la création poétique ? Notre rencontre sera l’occasion de croiser les paroles de plusieurs philosophes (Platon, Emerson, Weil…) avec celles de différents poètes (Hölderlin, Valéry, Michaux…), dans le but de nous questionner sur le sens de la poésie et sur notre lien à la beauté.
+        Marc Loopuyt a fait de sa vie une quête musicale en résidant longuement auprès de ses maîtres: Andalousie, Maroc, Turquie et Azerbaydjan. Il reçoit aussi d'eux tout un corpus de commentaires sapientiels sur les arcanes de la Musique. A Fès, sa fréquentation du grand islamologue Titus Burckhardt va l'inciter à systématiser ce recueil dans la perspective de l'unité transcendante des traditions esthétiques. Outre ses divers CD, il publie:" Un Luth mythique de Damas" ( Cité de la Musique) et:" le Voyage, la Musique et le Fil d'Ariane "( les Trois Colonnes, à paraître). Il propose ici une causerie fondée sur ces rencontres remarquables et sur des anecdotes précises et vivantes qui illustrent les principes et les méthodes d' intériorisation de l'art traditionnel. Cet exposé qui concerne la musique mais aussi l'art en généra, inclura de petits intermèdes joués sur le luth oriental.
         </p>
-        */}
+        
         {/* Image */}
         <img loading="lazy" src={Flyer7} alt="Flyer" className="object-contain rounded mb-6 w-full " />
 
