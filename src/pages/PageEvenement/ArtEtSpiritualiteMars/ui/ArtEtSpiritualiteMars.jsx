661645e5a15550db74637a49a55dda3ff55492cb
MTFL1 <m.ecomplus1@gmail.com>
2025-03-04 14:28:03 +0100

    add new event 29_03

---
+++ src/pages/PageEvenement/ArtEtSpiritualiteMars/ui/ArtEtSpiritualiteMars.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import BanniereEvenement from "./BanniereEvenementMars"
+import DetailsEvenements from "./DetailsevenementsMars"
+import FormWidget from "./FormWidgetMars";
+
+
+
+function ArtEtSpiritualiteMars() {
+  return (
+    <main>
+    <BanniereEvenement/>
+    <DetailsEvenements/>
+    <FormWidget/>
+    </main>
+  );
+}
+
+export default ArtEtSpiritualiteMars;
